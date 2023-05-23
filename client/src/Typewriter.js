14e607773cef7db5952a190faeaf3806c5b7aad0
Nathan Hino <awpertunistic@gmail.com>
2023-05-22 20:34:00 -0400

    Updates

---
+++ client/src/Typewriter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Typewriter.scss";
+
+const Typewriter = () => {
+  return (
+    <div>
+      <h1>
+        Hello, My name is Nathan, and I build...
+        <br />
+        <br />
+        <div className="c1">
+          <div className="type">Websites, Video Games, UI/UX</div>
+        </div>
+        <br />
+        <div className="c2">
+          <div className="type2">Using: C++, C#, Python</div>
+        </div>
+        <br />
+        <div className="c3">
+          <div className="type3">HTML, CSS, JavaScript, MySQL</div>
+        </div>
+        <br />
+      </h1>
+    </div>
+  );
+};
+
+export default Typewriter;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import ParticleCanvas from "./ParticleCanvas";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <ParticleCanvas />
   </React.StrictMode>
 );
